5229209a4bd3829592f77917e3f836c7cc4cfd19
unknown <wkot501@gmai.com>
2025-01-17 20:37:42 +0100

    Solver + Data update 0.1

--- solver.py
+++ solver.py
@@ -7,19 +7,24 @@ class Word:
         self.lettersNoPos = []
         self.results = []
         self.positions = []
+        self.noAvaliable = []
+        self.notOnPosition = {}
 
     def __str__(self):
         return ''.join([letter if letter is not None else '_' for letter in self.letters])
 
-    def input( self, letter, position = None):
+    def input( self, letter, position = None, available = True, notOnPosition = [] ):
         if position is not None:
             self.positions.append(position)
             if position >= self.length:
                 print( 'Position out of range' )
                 return
             self.letters[position] = letter
+        elif not available:
+            self.noAvaliable.append(letter)
         else:
-            self.lettersNoPos.append(letter)   
+            self.lettersNoPos.append(letter)
+            self.notOnPosition[letter] = notOnPosition   
         
         # self.search()
         
@@ -28,10 +33,74 @@ class Word:
             match(self.letters[0]):
                 case 'a':         
                     self.match_word(awords)  
+                case 'ą':
+                    self.match_word(ąwords)
                 case 'b':
                     self.match_word(bwords)
+                case 'c':
+                    self.match_word(cwords)
+                case 'ć':
+                    self.match_word(ćwords)
+                case 'd':
+                    self.match_word(dwords)
+                case 'e':
+                    self.match_word(ewords)
+                case 'ę':
+                    self.match_word(ęwords)
+                case 'f':
+                    self.match_word(fwords)
+                case 'g':
+                    self.match_word(gwords)
+                case 'h':
+                    self.match_word(hwords)
+                case 'i':
+                    self.match_word(iwords)
+                case 'j':
+                    self.match_word(jwords)
                 case 'k':
                     self.match_word(kwords)
+                case 'l':
+                    self.match_word(lwords)
+                case 'ł':
+                    self.match_word(łwords)
+                case 'm':
+                    self.match_word(mwords)
+                case 'n':
+                    self.match_word(nwords)
+                case 'ń':
+                    self.match_word(ńwords)
+                case 'o':
+                    self.match_word(owords)
+                case 'ó':
+                    self.match_word(ówords)
+                case 'p':
+                    self.match_word(pwords)
+                case 'q':
+                    self.match_word(qwords)
+                case 'r':
+                    self.match_word(rwords)
+                case 's':
+                    self.match_word(swords)
+                case 'ś':
+                    self.match_word(śwords)
+                case 't':
+                    self.match_word(twords)
+                case 'u':
+                    self.match_word(uwords)
+                case 'v':
+                    self.match_word(vwords)
+                case 'w':
+                    self.match_word(wwords)
+                case 'x':
+                    self.match_word(xwords)
+                case 'y':
+                    self.match_word(ywords)
+                case 'z':
+                    self.match_word(zwords)
+                case 'ź':
+                    self.match_word(źwords)
+                case 'ż':
+                    self.match_word(żwords)
                 case _:
                     print('Unknown letter')
         else:
@@ -40,22 +109,25 @@ class Word:
         return self.results
 
     def match_word( self, words ):
-        for word in words:
-            a = 0
-            for i in self.positions:
-                if word[i] == self.letters[i]:
-                    a += 1
-            if a == len(self.positions):
-                self.results.append(word)
-            
+        valid_words = []
+        letters_no_pos_set = set(self.lettersNoPos)
+        no_available_set = set(self.noAvaliable)
         
-        if self.lettersNoPos != []:
-            for word in self.results[:]:
-                cutted = self.remove_positions(word, self.positions)
-
-                for letter in self.lettersNoPos:
-                    if letter not in cutted and word in self.results:
-                        self.results.remove(word)
+        for word in words:
+            if all(word[i] == self.letters[i] for i in self.positions):
+                valid_words.append(word)
+        
+        if self.lettersNoPos:
+            valid_words = [word for word in valid_words if letters_no_pos_set.issubset(self.remove_positions(word, self.positions))]
+        
+        if self.notOnPosition:
+            for letter, positions in self.notOnPosition.items():
+                valid_words = [word for word in valid_words if all(word[pos] != letter for pos in positions)]
+        
+        if self.noAvaliable:
+            valid_words = [word for word in valid_words if all(word.count(letter) == 1 for letter in letters_no_pos_set) and all(word.count(letter) <= 1 for letter in no_available_set) and all(word.count(letter) <= 1 for letter in self.noAvaliable)]
+        
+        self.results = valid_words
 
     def remove_positions(self, word, positions):
         return ''.join([char for idx, char in enumerate(word) if idx not in positions])
@@ -63,7 +135,11 @@ class Word:
 
 a = Word()
 
-a.input('k', 0)
-a.input('a')
+a.input('i', notOnPosition=[3])
+a.input('b', notOnPosition=[2,3])
+a.input('r', 4)
+a.input('z')
+
+
 
 print(a.search())
